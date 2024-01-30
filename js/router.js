28a6d1038140cf3c4e79c451853a9e9371230f4d
karlwoien <karlwoien@gmail.com>
2024-01-30 15:24:34 +0100

    Create router.js

---
+++ js/router.js
@@ -0,0 +1,38 @@
+// Function to only run certain JS on certain pages
+
+// Home: Latest blog carousel
+// Blog: All blog posts 
+// Blog post: blog post data specific
+// Contact: Forum submission listener
+
+
+export function router () {
+    switch(window.location.pathname) {
+        case "/":
+        case "/index.html":
+            console.log("Home page");
+            break;
+        case "/about":
+        case "about/":
+        case "/about/index.html":
+            console.log("About page");
+            break;
+        case "/contact":
+        case "/contact/":
+        case "/contact/index.html":
+            console.log("contact page");
+            break;
+        case "/blog":
+        case "/blog/":
+        case "/blog/index.html":
+            console.log("blog page");
+            break;
+        case "/blog/blog-post":
+        case "/blog/blog-post/":
+        case "/blog/blog-post/index.html":
+            console.log("blog-post page");
+            break;
+        default:
+            console.log("404 - not found")
+    }
+}
