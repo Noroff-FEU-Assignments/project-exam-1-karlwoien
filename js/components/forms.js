58bd3e1b1872fba2f038d4f7df6f76ef0b5a4364
karlwoien <karlwoien@gmail.com>
2024-02-08 11:41:59 +0100

    Create forms.js

---
+++ js/components/forms.js
@@ -0,0 +1,11 @@
+
+// Validation of contact form
+
+function inputLength (input, minlength) {
+    if (input.length >= minlength) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
