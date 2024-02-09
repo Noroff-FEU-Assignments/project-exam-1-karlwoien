c9839c66f04c8c1178e6080c2d1950d051988639
karlwoien <karlwoien@gmail.com>
2024-02-09 13:56:49 +0100

    update

--- js/components/carousel.js
+++ js/components/carousel.js
@@ -1,8 +1,6 @@
 /* Carousel for both home and about page
 Inspired by Medium: https://medium.com/@algopoint.ltd/how-to-create-a-slideshow-carousel-using-html-css-js-7ab0561b39b3*/
 
-
-
 // Slideshow functionality
 
 export function slideshow () {
